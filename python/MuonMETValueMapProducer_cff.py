87b666c0a30e8c2940e718fa09ce4083e3b773b1
Frank Golf <sha1-e884805f6c312aa2407c5005ebbb36cbb0c23396@cern.ch>
2010-02-04 19:01:24 +0000

    update value map producer to use default muon fit by default

--- python/MuonMETValueMapProducer_cff.py
+++ python/MuonMETValueMapProducer_cff.py
@@ -11,14 +11,12 @@ muonMETValueMapProducer = cms.EDProducer("MuonMETValueMapProducer",
      useRecHits   = cms.bool(False), #if True, will use deposits in 3x3 recHits
      useHO        = cms.bool(False), #if True, will correct for deposits in HO
      isAlsoTkMu   = cms.bool(True),     #does the mu have to be a tracker mu?
-     towerEtThreshold = cms.double(0.5), #default MET calculated using towers with Et > 0.5 GeV only
+     towerEtThreshold = cms.double(0.3), #default MET calculated using towers with Et > 0.5 GeV only
      minPt        = cms.double(10.0),   #min global Mu Pt is 10 GeV
      maxEta       = cms.double(2.4),    #max global |Eta| is 2.5
      maxNormChi2  = cms.double(10.0),   #max global chi2/ndof
      maxd0        = cms.double(0.2),    #max global d0
-     minnHits     = cms.int32(11),      #minimum # of si hits
-     qOverPErr    = cms.double(999999.9),
-     delPtOverPt  = cms.double(0.2) # if the delta(globalpt, trackpt)/trackpt > 0.2, use track pt
+     minnHits     = cms.int32(11)      #minimum # of si hits
 )
 muonMETValueMapProducer.TrackAssociatorParameters.useEcal = False
 muonMETValueMapProducer.TrackAssociatorParameters.useHcal = False
