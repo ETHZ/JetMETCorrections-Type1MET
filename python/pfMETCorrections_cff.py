09794ce0eb63b851d88fcb359fbdf871f27b5477
cmsbuild <>
2012-05-30 20:44:50 +0000

    This commit was manufactured by cvs2git to create tag 'JetMETCorrections- Type1MET-V04-06-06'.
    Sprout from master 2012-05-01 11:34:43 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> 'minor restructuring of sequence'
    Cherrypick from master 2012-05-30 20:44:49 UTC Tai Sakuma <sha1-83aadde34f3854f55faaed54e7ebb52fa93fe00c@cern.ch> 'turned off Type-0 for pfType1CorrectedMet and pfType1p2CorrectedMet':
        python/pfMETCorrections_cff.py

--- python/pfMETCorrections_cff.py
+++ python/pfMETCorrections_cff.py
@@ -4,23 +4,6 @@ import FWCore.ParameterSet.Config as cms
 from JetMETCorrections.Configuration.JetCorrectionServices_cff import *
 
 #--------------------------------------------------------------------------------
-# produce rho parameters needed for L1FastJet corrections
-from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets
-kt6PFJets = kt4PFJets.clone(
-    src = cms.InputTag('particleFlow'),
-    rParam = cms.double(0.6),
-    doRhoFastjet = cms.bool(True),
-    Rho_EtaMax = cms.double(2.5)
-)
-
-# CV: need to rerun 'ak5PFJets' module with jet area computation enabled,
-#     since it has not been enabled per default in CMSSW_4_2_x
-#    (if the jet area of 'ak5PFJets' is zero, the L1FastjetCorrector::correction function always returns 1.0)
-from RecoJets.JetProducers.ak5PFJets_cfi import ak5PFJets
-ak5PFJets.doAreaFastjet = cms.bool(True)
-#--------------------------------------------------------------------------------
-
-#--------------------------------------------------------------------------------
 # select PFCandidates ("unclustered energy") not within jets
 # for Type 2 MET correction
 from CommonTools.ParticleFlow.TopProjectors.pfNoJet_cfi import pfNoJet
@@ -65,7 +48,7 @@ pfchsMETcorr = cms.EDProducer("PFchsMETcorrInputProducer",
 # use MET corrections to produce Type 1 / Type 1 + 2 corrected PFMET objects
 pfType1CorrectedMet = cms.EDProducer("CorrectedPFMETProducer",
     src = cms.InputTag('pfMet'),
-    applyType0Corrections = cms.bool(True),
+    applyType0Corrections = cms.bool(False),
     srcCHSSums = cms.VInputTag(
         cms.InputTag('pfchsMETcorr', 'type0')
     ),
@@ -79,7 +62,7 @@ pfType1CorrectedMet = cms.EDProducer("CorrectedPFMETProducer",
 
 pfType1p2CorrectedMet = cms.EDProducer("CorrectedPFMETProducer",
     src = cms.InputTag('pfMet'),
-    applyType0Corrections = cms.bool(True),
+    applyType0Corrections = cms.bool(False),
     srcCHSSums = cms.VInputTag(
         cms.InputTag('pfchsMETcorr', 'type0')
     ),
@@ -104,9 +87,7 @@ pfType1p2CorrectedMet = cms.EDProducer("CorrectedPFMETProducer",
 #--------------------------------------------------------------------------------
 # define sequence to run all modules
 producePFMETCorrections = cms.Sequence(
-    kt6PFJets
-   * ak5PFJets
-   * pfCandsNotInJet
+    pfCandsNotInJet
    * pfJetMETcorr
    * pfCandMETcorr
    * pfchsMETcorr
