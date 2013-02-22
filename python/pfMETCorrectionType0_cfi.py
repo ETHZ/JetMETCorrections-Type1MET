ab01caa1d46c7f86c426833a2a85bbdbb592336d
cmsbuild <>
2013-02-22 15:38:48 +0000

    This commit was manufactured by cvs2git to create tag 'JetMETCorrections- Type1MET-b5_3_X_cvMEtCorr_2013Feb22'.
    Sprout from master 2013-02-22 15:38:44 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> '- synchronization how Type-1 corrections are applied to CaloMEt and PFMET'
    Cherrypick from master 2012-05-01 11:34:43 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> 'minor restructuring of sequence':
        BuildFile.xml
        plugins/Type0PFMETcorrInputProducer.cc
        python/pfMETCorrectionType0_cfi.py
    Cherrypick from JetMETCorrections-Type1MET-B5_3_X 2013-02-22 15:30:31 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> 'added option to apply residual correction to unclustered energy':
        python/pfMETCorrections_cff.py
    Delete:
        plugins/CorrectedCaloMETProducer2.cc
        plugins/CorrectedPFMETProducer2.cc
        plugins/ScaleCorrMETData.cc
        plugins/Type2CorrectionProducer.cc

--- python/pfMETCorrectionType0_cfi.py
+++ python/pfMETCorrectionType0_cfi.py
@@ -22,11 +22,12 @@ selectedPrimaryVertexHighestPtTrackSumForPFMEtCorrType0 = cms.EDFilter("PATSingl
 from RecoParticleFlow.PFTracking.particleFlowDisplacedVertex_cfi import particleFlowDisplacedVertex
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
 
+from CommonTools.RecoUtils.pf_pu_assomap_cfi import Tracks2Vertex
+trackToVertexAssociation = Tracks2Vertex.clone()
+
 from CommonTools.RecoUtils.pfcand_assomap_cfi import PFCandAssoMap
 pfCandidateToVertexAssociation = PFCandAssoMap.clone(
-    PFCandidateCollection = cms.InputTag('particleFlow'),
-    UseBeamSpotCompatibility = cms.untracked.bool(True),
-    ignoreMissingCollection = cms.bool(True)
+    VertexTrackAssociationMap = cms.InputTag('trackToVertexAssociation')
 )
 #--------------------------------------------------------------------------------
 
@@ -39,9 +40,9 @@ pfMETcorrType0 = cms.EDProducer("Type0PFMETcorrInputProducer",
     correction = cms.PSet(
         formula = cms.string("-([0] + [1]*x)*(1.0 + TMath::Erf(-[2]*TMath::Power(x, [3])))"),
         par0 = cms.double(0.),
-        par1 = cms.double(-0.703151),
-        par2 = cms.double(0.0303531),
-        par3 = cms.double(0.909209)          
+        par1 = cms.double(-0.710135),
+        par2 = cms.double(0.0870503),
+        par3 = cms.double(0.621243)          
     ),
     minDz = cms.double(0.2) # [cm], minimum distance required between pile-up vertices and "hard scatter" vertex
 )   
@@ -51,6 +52,7 @@ type0PFMEtCorrectionPFCandToVertexAssociation = cms.Sequence(
     selectedVerticesForPFMEtCorrType0
    * selectedPrimaryVertexHighestPtTrackSumForPFMEtCorrType0
    * particleFlowDisplacedVertex
+   * trackToVertexAssociation
    * pfCandidateToVertexAssociation
 )
 
