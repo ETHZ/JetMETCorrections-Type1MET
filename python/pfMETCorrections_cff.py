6544eb0cf13fe5f0e3061a22da4848d09ae40574
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2013-05-13 16:29:21 +0000

    added option to apply residual correction to unclustered energy from TXT file

--- python/pfMETCorrections_cff.py
+++ python/pfMETCorrections_cff.py
@@ -23,6 +23,8 @@ pfJetMETcorr = cms.EDProducer("PFJetMETcorrInputProducer",
     type1JetPtThreshold = cms.double(10.0),
     type2ResidualCorrLabel = cms.string(""),
     type2ResidualCorrEtaMax = cms.double(9.9),
+    type2ResidualCorrOffset = cms.double(0.),
+    isMC = cms.bool(False), # CV: only used to decide whether to apply "unclustered energy" calibration to MC or Data     
     skipEM = cms.bool(True),
     skipEMfractionThreshold = cms.double(0.90),
     skipMuons = cms.bool(True),
@@ -36,7 +38,8 @@ pfCandMETcorr = cms.EDProducer("PFCandMETcorrInputProducer",
     src = cms.InputTag('pfCandsNotInJet'),
     residualCorrLabel = cms.string(""),
     residualCorrEtaMax = cms.double(9.9),
-    residualCorrOffset = cms.double(0.)                         
+    residualCorrOffset = cms.double(0.),
+    isMC = cms.bool(False) # CV: only used to decide whether to apply "unclustered energy" calibration to MC or Data
 )   
 #--------------------------------------------------------------------------------
 
