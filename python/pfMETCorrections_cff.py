293a0a8fcd93df5a610aef7f446ea0231e87c634
cmsbuild <>
2013-06-02 14:58:22 +0000

    This commit was manufactured by cvs2git to create tag 'JetMETCorrections- Type1MET-b5_3_X_cvMEtCorr_2013Jun02'.
    Sprout from master 2013-06-02 14:58:21 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> 'added unclustered energy calibration parameters (for Prompt Reco data/old JEC)'
    Cherrypick from master 2012-05-01 11:34:43 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> 'minor restructuring of sequence':
        BuildFile.xml
        interface/TauMETAlgo.h
        interface/Type1METAlgo.h
        plugins/TauMET.cc
        plugins/TauMET.h
        plugins/Type0PFMETcorrInputProducer.cc
        plugins/Type1MET.cc
        plugins/Type1MET.h
        python/MetType1Corrections_cff.py
        python/TauMetCorrections_cff.py
        python/pfMETCorrectionType0_cfi.py
        src/TauMETAlgo.cc
        src/Type1METAlgo.cc
    Cherrypick from JetMETCorrections-Type1MET-B5_3_X 2013-05-13 16:29:21 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> 'added option to apply residual correction to unclustered energy from TXT file':
        python/pfMETCorrections_cff.py
    Delete:
        plugins/CorrectedCaloMETProducer2.cc
        plugins/CorrectedPFMETProducer2.cc
        plugins/ScaleCorrMETData.cc
        plugins/Type2CorrectionProducer.cc

--- python/pfMETCorrections_cff.py
+++ python/pfMETCorrections_cff.py
@@ -1,37 +1,20 @@
 import FWCore.ParameterSet.Config as cms
-# $Id: pfMETCorrections_cff.py,v 1.14 2013/05/14 23:18:39 sakuma Exp $
 
-##____________________________________________________________________________||
+# load jet energy correction parameters
 from JetMETCorrections.Configuration.JetCorrectionServices_cff import *
 
-##____________________________________________________________________________||
+#--------------------------------------------------------------------------------
 # select PFCandidates ("unclustered energy") not within jets
 # for Type 2 MET correction
 from CommonTools.ParticleFlow.TopProjectors.pfNoJet_cfi import pfNoJet
-# the new TopProjectors now work with Ptrs
-# a conversion is needed if objects are not available
-# add them upfront of the sequence
-ak5PFJetsPtrs = cms.EDProducer("PFJetFwdPtrProducer",
-   src = cms.InputTag("ak5PFJets")
+pfCandsNotInJet = pfNoJet.clone(
+    topCollection = cms.InputTag('ak5PFJets'),
+    bottomCollection = cms.InputTag('particleFlow')
 )
-# this one is needed only if the input file doesn't have it
-# solved automatically with unscheduled execution
-from RecoParticleFlow.PFProducer.pfLinker_cff import particleFlowPtrs
-# particleFlowPtrs = cms.EDProducer("PFCandidateFwdPtrProducer",
-#    src = cms.InputTag("particleFlow")
-# )
-# FIXME: THIS IS A WASTE, BUT NOT CLEAR HOW TO FIX IT CLEANLY: the module
-# downstream operates with View<reco::Candidate>, I wish one could read
-# it from std::vector<PFCandidateFwdPtr> directly
-pfCandsNotInJetPtrs = pfNoJet.clone(
-    topCollection = cms.InputTag('ak5PFJetsPtrs'),
-    bottomCollection = cms.InputTag('particleFlowPtrs')
-)
-pfCandsNotInJet = cms.EDProducer("PFCandidateFromFwdPtrProducer",
-    src = cms.InputTag("pfCandsNotInJetPtrs")
-)
+#--------------------------------------------------------------------------------
 
-##____________________________________________________________________________||
+#--------------------------------------------------------------------------------
+# produce Type 1 + 2 MET corrections for PFJets
 pfJetMETcorr = cms.EDProducer("PFJetMETcorrInputProducer",
     src = cms.InputTag('ak5PFJets'),
     offsetCorrLabel = cms.string("ak5PFL1Fastjet"),
@@ -41,14 +24,16 @@ pfJetMETcorr = cms.EDProducer("PFJetMETcorrInputProducer",
     type2ResidualCorrLabel = cms.string(""),
     type2ResidualCorrEtaMax = cms.double(9.9),
     type2ResidualCorrOffset = cms.double(0.),
-    isMC = cms.bool(False), # CV: only used to decide whether to apply "unclustered energy" calibration to MC or Data                               
+    isMC = cms.bool(False), # CV: only used to decide whether to apply "unclustered energy" calibration to MC or Data     
     skipEM = cms.bool(True),
     skipEMfractionThreshold = cms.double(0.90),
     skipMuons = cms.bool(True),
     skipMuonSelection = cms.string("isGlobalMuon | isStandAloneMuon")
 )                                         
+#--------------------------------------------------------------------------------
 
-##____________________________________________________________________________||
+#--------------------------------------------------------------------------------
+# produce Type 2 MET corrections for selected PFCandidates
 pfCandMETcorr = cms.EDProducer("PFCandMETcorrInputProducer",
     src = cms.InputTag('pfCandsNotInJet'),
     residualCorrLabel = cms.string(""),
@@ -56,15 +41,19 @@ pfCandMETcorr = cms.EDProducer("PFCandMETcorrInputProducer",
     residualCorrOffset = cms.double(0.),
     isMC = cms.bool(False) # CV: only used to decide whether to apply "unclustered energy" calibration to MC or Data
 )   
+#--------------------------------------------------------------------------------
 
-##____________________________________________________________________________||
+#--------------------------------------------------------------------------------
+# produce Type 0 MET corrections for selected vertices
 pfchsMETcorr = cms.EDProducer("PFchsMETcorrInputProducer",
     src = cms.InputTag('offlinePrimaryVertices'),
     goodVtxNdof = cms.uint32(4),
     goodVtxZ = cms.double(24)
 )   
+#--------------------------------------------------------------------------------
 
-##____________________________________________________________________________||
+#--------------------------------------------------------------------------------
+# use MET corrections to produce Type 1 / Type 1 + 2 corrected PFMET objects
 pfType1CorrectedMet = cms.EDProducer("CorrectedPFMETProducer",
     src = cms.InputTag('pfMet'),
     applyType0Corrections = cms.bool(False),
@@ -79,7 +68,6 @@ pfType1CorrectedMet = cms.EDProducer("CorrectedPFMETProducer",
     applyType2Corrections = cms.bool(False)
 )   
 
-##____________________________________________________________________________||
 pfType1p2CorrectedMet = cms.EDProducer("CorrectedPFMETProducer",
     src = cms.InputTag('pfMet'),
     applyType0Corrections = cms.bool(False),
@@ -102,18 +90,16 @@ pfType1p2CorrectedMet = cms.EDProducer("CorrectedPFMETProducer",
         A = cms.double(1.4)
     )
 )   
+#--------------------------------------------------------------------------------
 
-##____________________________________________________________________________||
+#--------------------------------------------------------------------------------
+# define sequence to run all modules
 producePFMETCorrections = cms.Sequence(
-    ak5PFJetsPtrs
-   * particleFlowPtrs 
-   * pfCandsNotInJetPtrs
-   * pfCandsNotInJet
+    pfCandsNotInJet
    * pfJetMETcorr
    * pfCandMETcorr
    * pfchsMETcorr
    * pfType1CorrectedMet
    * pfType1p2CorrectedMet
 )
-
-##____________________________________________________________________________||
+#--------------------------------------------------------------------------------
